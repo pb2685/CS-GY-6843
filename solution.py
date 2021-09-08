34ae8c6cb2a2e828e913de0df466c92d2c0e3588
pb2685 <90349172+pb2685@users.noreply.github.com>
2021-09-09 05:12:00 +0800

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,4 @@
-
-### welcome_assignment_answers
-### Input - All eight questions given in the assignment.
-### Output - The right answer for the specific question.
-
-
-
-
 def welcome_assignment_answers(question):
-    #The student doesn't have to follow the skeleton for this assignment.
-    #Another way to implement is using a "case" statements similar to C.
     if question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
         answer = "mTLS"
     elif question == "Are encoding and encryption the same? - Yes/No":
@@ -26,9 +16,8 @@ def welcome_assignment_answers(question):
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
         answer = 4
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number" :
-        answer = 3
+        answer = 4
     return(answer)
-# Complete all the questions.
 
 
  
